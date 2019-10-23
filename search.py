5af01e0633966a4061c2343c8019ebc1958fb895
Peter Bakhirev <pbkhrv@pm.me>
2019-10-22 23:56:40 -0400

    lint search.py

--- search.py
+++ search.py
@@ -1,3 +1,9 @@
+"""
+Note searching functionality
+
+- Uses `grep` to search note contents
+- Uses `find` to search note titles
+"""
 import subprocess
 import re
 import os
@@ -5,7 +11,13 @@ from typing import NamedTuple
 from functools import partial
 
 
-class SearchResultItem(NamedTuple):
+class SearchResultItem(NamedTuple):  # pylint: disable=too-few-public-methods
+    """
+    Note search result item
+
+    *_lower properties used for sort key generation
+    """
+
     filename: str
     filename_lower: str
     match_content: str
@@ -14,12 +26,22 @@ class SearchResultItem(NamedTuple):
 
 
 class SubprocessError(Exception):
+    """
+    Subprocess run didn't return expected exit code
+    """
+
     pass
 
 
 def grep_dir(path, file_exts, pattern):
+    """
+    Call `grep` recursively on a directory and return matching filenames and
+    first matching line of each file
+
+    Only include files with certain extensions.
+    """
     include_globs = ["--include=*.{}".format(e) for e in file_exts]
-    cp = subprocess.run(
+    ret = subprocess.run(
         [
             "grep",
             "--with-filename",
@@ -35,27 +57,29 @@ def grep_dir(path, file_exts, pattern):
         stderr=subprocess.PIPE,
     )
 
-    if cp.returncode == 2:
-        raise SubprocessError(cp.stderr.decode("utf-8"))
+    if ret.returncode == 2:
+        raise SubprocessError(ret.stderr.decode("utf-8"))
     else:
-        s = cp.stdout.decode("utf-8")
+        out = ret.stdout.decode("utf-8")
         # Can't use .splitlines below because some of my files contain lines with weird linebreaks
-        lines = s.split("\n")
+        lines = out.split("\n")
         return [tuple(l.split("\x00", maxsplit=1)) for l in lines if l]
 
 
-def escape_regex(s):
-    return re.sub(r"([\?\*\.\\\{\}\[\]\+])", r"\\\1", s)
-
-
 def file_exts_to_regex(exts):
-    globs = ["\\.{}".format(escape_regex(e)) for e in exts]
+    """
+    Turn list of file extensions into one regex to be used with `find`
+    """
+    globs = ["\\.{}".format(re.escape(e)) for e in exts]
     return ".+({})$".format("|".join(globs))
 
 
 def find_dir(path, file_exts, query_args):
+    """
+    Execute `find` on a directory and find all files of particular extension matching a file pattern
+    """
     name_glob = "*" + "*".join(query_args) + "*"
-    cp = subprocess.run(
+    ret = subprocess.run(
         [
             "find",
             path,
@@ -72,29 +96,33 @@ def find_dir(path, file_exts, query_args):
         stderr=subprocess.PIPE,
     )
 
-    if cp.returncode != 0:
-        raise SubprocessError(cp.stderr.decode("utf-8"))
+    if ret.returncode != 0:
+        raise SubprocessError(ret.stderr.decode("utf-8"))
     else:
-        s = cp.stdout.decode("utf-8")
-        return s.splitlines()
+        return ret.stdout.decode("utf-8").splitlines()
 
 
-def summarized_content_match(s, ctx_word, ctx_len):
-    i = s.find(ctx_word)
+def summarized_content_match(text, ctx_word, ctx_len):
+    """
+    Summarize a line of text by leaving ctx_len characters around the ctx_word and trimming the rest
+    """
+    i = text.find(ctx_word)
     if i == -1:
-        return s
-    else:
-        start = max(0, i - ctx_len)
-        end = min(len(s), i + len(ctx_word) + ctx_len)
-        ctx = "..." if start > 0 else ""
-        ctx += s[start:end]
-        ctx += "..." if end < len(s) else ""
-        return ctx
+        return text
+    start = max(0, i - ctx_len)
+    end = min(len(text), i + len(ctx_word) + ctx_len)
+    ctx = "..." if start > 0 else ""
+    ctx += text[start:end]
+    ctx += "..." if end < len(text) else ""
+    return ctx
 
 
 def search_note_file_contents(path, file_exts, query):
+    """
+    Call `grep` and turn results into SearchResultItem's
+    """
     args = query.lower().split(" ")
-    pattern = ".+".join(escape_regex(a) for a in args)
+    pattern = ".+".join(re.escape(a) for a in args)
     full_path = os.path.expanduser(path)
     grep_matches = grep_dir(full_path, file_exts, pattern)
     matches = []
@@ -114,6 +142,9 @@ def search_note_file_contents(path, file_exts, query):
 
 
 def search_note_file_titles(path, file_exts, query):
+    """
+    Call `find` and turn results into SearchResultItem's
+    """
     args = query.lower().split(" ")
     full_path = os.path.expanduser(path)
     find_matches = find_dir(full_path, file_exts, args)
@@ -137,9 +168,9 @@ def match_sort_key(word_boundary_regex, match):
     - has text summary
     - alpha-numeric sort of filenames
     """
-    fm = word_boundary_regex.search(match.filename_lower)
+    word_matched = word_boundary_regex.search(match.filename_lower)
     return (
-        fm.start() if fm else 2**10,
+        word_matched.start() if word_matched else 2 ** 10,
         0 if word_boundary_regex.search(match.match_content_lower) is not None else 1,
         0 if match.match_summary else 1,
         match.filename_lower,
@@ -147,11 +178,14 @@ def match_sort_key(word_boundary_regex, match):
 
 
 def search_notes(path, file_exts, query):
+    """
+    Search note contents and titles, combine, dedup and sort results.
+    """
     grep_matches = search_note_file_contents(path, file_exts, query)
     find_matches = search_note_file_titles(path, file_exts, query)
     # dont include `find` matches for the same fn that appeared in `grep` matches
     grep_fns = set(m.filename for m in grep_matches)
     matches = grep_matches + [m for m in find_matches if m.filename not in grep_fns]
     args = query.split(" ")
-    word_boundary_regex = re.compile("\\b{}\\b".format(re.escape(args[0])))
+    word_boundary_regex = re.compile("\\b{}".format(re.escape(args[0])))
     return list(sorted(matches, key=partial(match_sort_key, word_boundary_regex)))
